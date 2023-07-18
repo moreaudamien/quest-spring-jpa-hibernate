6b95ecf2f9d7da0e0983fee04952dda3db130502
moreaudamien <moreau_student2023@wilder.school>
2023-07-18 15:31:08 +0200

    challenge

--- src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
@@ -1,6 +1,11 @@
 package com.wildcodeschool.wildandwizard.controller;
 
 import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.repository.SchoolRepository;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,20 +18,31 @@ public class SchoolController {
 
     // TODO : get school repository by dependency injection
 
+    @Autowired
+    private SchoolRepository schoolRepository;
+
     @GetMapping("/schools")
     public String getAll(Model model) {
 
         // TODO : find all schools
-
+        model.addAttribute("schools", schoolRepository.findAll());
         return "schools";
     }
 
     @GetMapping("/school")
     public String getSchool(Model model,
-                            @RequestParam(required = false) Long id) {
+            @RequestParam(required = false) Long id) {
 
         // TODO : find one school by id
 
+        School school = new School();
+        if (id != null) {
+            Optional<School> optionalSchool = schoolRepository.findById(id);
+            if (optionalSchool.isPresent()) {
+                school = optionalSchool.get();
+            }
+        }
+        model.addAttribute("school", school);
         return "school";
     }
 
@@ -34,7 +50,7 @@ public class SchoolController {
     public String postSchool(@ModelAttribute School school) {
 
         // TODO : create or update a school
-
+        schoolRepository.save(school);
         return "redirect:/schools";
     }
 
@@ -42,7 +58,7 @@ public class SchoolController {
     public String deleteSchool(@RequestParam Long id) {
 
         // TODO : delete a school
-
+        schoolRepository.deleteById(id);
         return "redirect:/schools";
     }
 }
